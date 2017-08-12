9b532802f4e8815e9e932c2cbbda6bd613d6d79c
wmr1 <wmr@cin.ufpe.br>
2017-08-12 01:25:09 -0300

    adicionado script de criação de boids, iniciado script de comportamento individual

---
+++ Assets/boidBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boidBehaviour : MonoBehaviour {
+
+	public Vector3 BOID_POS;
+	private GameObject[] Boids;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//find screen behaviour object
+		GameObject sceneBehaviour = GameObject.Find("SceneBehaviour");
+		//get boids vector
+		boidsGeneration boidsGenerationScript = sceneBehaviour.GetComponent<boidsGeneration>();
+		Boids = boidsGenerationScript.Boids;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject boid in Boids)
+		{
+			Vector3 position = new Vector3(
+				Random.Range(-0.50f, 0.50f), 
+				Random.Range(-0.50f, 0.50f), 
+				Random.Range(-0.50f, 0.50f));
+			
+			boid.transform.position += position;
+		}
+	}
+}
---
+++ Assets/boidsGeneration.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boidsGeneration : MonoBehaviour {
+	//store gameObject reference on global contex
+	public int NumberOfBoids;
+	public GameObject[] Boids;
+
+
+	GameObject CreateBoid()
+	{
+		//spawn boid
+		GameObject boid = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		//change components
+		Vector3 position = new Vector3(
+			Random.Range(-10.0f, 10.0f), 
+			Random.Range(-10.0f, 10.0f), 
+			Random.Range(-10.0f, 10.0f));
+		boid.transform.position = position;
+		boid.name = "boid";
+		//add components
+		boid.AddComponent<boidBehaviour>();
+
+		return boid;
+	}
+
+	void Start()
+	{
+		Boids = new GameObject[NumberOfBoids];
+		for (int i = 0; i < NumberOfBoids; i++)
+			Boids [i] = CreateBoid ();
+
+	}
+}
